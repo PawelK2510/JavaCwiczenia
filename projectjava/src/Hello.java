f927c9f254452c27a1c847d5478b4a22fb3794d9
Paweł K <pawel20@gmail.com>
2025-09-10 11:30:45 +0200

    Stworzenie projektu

---
+++ projectjava/src/Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Paweł 5cI");
+        // Komentowanie wielu linijek -ztrl + /
+        /*
+        To jest komentarz wieloliniowy.
+
+         */
+        int a = 5, b = 2;
+        System.out.println("Wynik dodawania:" + (a+b));
+        float wynik = 0;
+        wynik = (float)a/b;
+        System.out.println(wynik);
+        System.out.println("Tu jest coś\t\t\t\t\t A tu co innego");
+        System.out.println("Zdanie\n");
+        System.out.println("Drugie zdanie");
+        System.out.println("Do robienia znaków specjalnych używamy znaku \\");
+
+    }
+}
--- projectjava/src/Main.java
+++ projectjava/src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    boolean zm = true;
+    if (!zm) {
+        System.out.println("Zmienna to prawda");
+    }else {
+        System.out.println("Zmienna to fałsz");
+    }
+    String napis = "Napis to jest.";
+    napis = napis + " A t o jest kolejny napis";
+    System.out.println(napis);
+    System.out.println('A' + 0);
+    System.out.println("Pole koła to: " + (3*4*3.14));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
